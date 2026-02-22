479c617eed8ea26268853a8aa967ca1cf2ff4d1b
Роберт <prokotrob@gmail.com>
2026-02-22 13:39:05 +0700

    Simplify eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,47 +2,8 @@ import stylistic from '@stylistic/eslint-plugin';
 
 export default [
   {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        console: 'readonly',
-        process: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly',
-        describe: 'readonly',
-        it: 'readonly',
-        test: 'readonly',
-        expect: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
-        jest: 'readonly',
-      },
-    },
     plugins: {
-      '@stylistic': stylistic,
-    },
-    rules: {
-      'no-console': 'off',
-      'import/extensions': ['error', 'ignorePackages', { js: 'always' }],
-      'import/prefer-default-export': 'off',
-      'no-underscore-dangle': 'off',
-      'class-methods-use-this': 'off',
-      'max-classes-per-file': 'off',
-      '@stylistic/indent': ['error', 2],
-      '@stylistic/quotes': ['error', 'single'],
-      '@stylistic/semi': ['error', 'always'],
-      '@stylistic/comma-dangle': ['error', 'always-multiline'],
-      '@stylistic/object-curly-spacing': ['error', 'always'],
-      '@stylistic/array-bracket-spacing': ['error', 'never'],
-      '@stylistic/arrow-parens': ['error', 'always'],
-      '@stylistic/brace-style': ['error', '1tbs'],
-      '@stylistic/eol-last': ['error', 'always'],
-      '@stylistic/no-trailing-spaces': 'error',
-      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
-    },
-  },
+      '@stylistic': stylistic
+    }
+  }
 ];
